2e934db05b413550f0720aeb67aa32bacc41f389
Jose Uriel Saenz Cuellar <54480651+hackrey21@users.noreply.github.com>
2025-09-02 10:45:07 -0500

    Update shake.js
    lkn;ln;oi

--- shake.js
+++ shake.js
@@ -1,39 +1,32 @@
-if (typeof DeviceMotionEvent.requestPermission === 'function') {
-    DeviceMotionEvent.requestPermission()
-        .then(permissionState => {
-            if (permissionState === 'granted') {
-                let shakeThreshold = 15; // sensibilidad de la sacudida
-                let lastX, lastY, lastZ;
-                let lastTime = new Date().getTime();
+let shakeThreshold = 15; // sensibilidad
+let lastX, lastY, lastZ;
+let lastTime = new Date().getTime();
 
-                window.addEventListener("devicemotion", function (event) {
-                    let acceleration = event.accelerationIncludingGravity;
-                    let currentTime = new Date().getTime();
-                    let timeDifference = currentTime - lastTime;
-
-                    if (timeDifference > 100) { // cada 100ms
-                        let deltaX = 0, deltaY = 0, deltaZ = 0;
-
-                        if (lastX !== undefined) {
-                            deltaX = Math.abs(acceleration.x - lastX);
-                            deltaY = Math.abs(acceleration.y - lastY);
-                            deltaZ = Math.abs(acceleration.z - lastZ);
-                        }
-
-                        if (deltaX + deltaY + deltaZ > shakeThreshold) {
-                            mostrarPoseRandom();
-                        }
-
-                        lastX = acceleration.x;
-                        lastY = acceleration.y;
-                        lastZ = acceleration.z;
-                        lastTime = currentTime;
-                    }
-                });
-
-            }
-        })
-        .catch(console.error);
+function mostrarPoseRandom() {
+    alert("¡Se detectó la sacudida!"); // prueba
 }
 
+window.addEventListener("devicemotion", function(event) {
+    let acceleration = event.accelerationIncludingGravity;
+    let currentTime = new Date().getTime();
+    let timeDifference = currentTime - lastTime;
 
+    if (timeDifference > 100) { // cada 100ms
+        let deltaX = 0, deltaY = 0, deltaZ = 0;
+
+        if (lastX !== undefined) {
+            deltaX = Math.abs(acceleration.x - lastX);
+            deltaY = Math.abs(acceleration.y - lastY);
+            deltaZ = Math.abs(acceleration.z - lastZ);
+        }
+
+        if (deltaX + deltaY + deltaZ > shakeThreshold) {
+            mostrarPoseRandom();
+        }
+
+        lastX = acceleration.x;
+        lastY = acceleration.y;
+        lastZ = acceleration.z;
+        lastTime = currentTime;
+    }
+});
